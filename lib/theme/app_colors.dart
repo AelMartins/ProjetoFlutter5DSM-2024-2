5f6d8cf2b8d519554524d4fcf74658387cac3666
Ael Martins <oneapcwork@gmail.com>
2024-09-29 11:49:49 -0300

    Definido a paleta de cores que será utilizado no projeto
    Foi criado um arquivo com o objetivo de armazenar todas as variaveis de ambiente das cores da aplicação

---
+++ lib/theme/app_colors.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // GLOBAL
+  static const Color standardTextColor = Colors.black;
+  static const Color standardBackgroundColor = Colors.white;
+  static const Color standardBarColor = Colors.white;
+
+  // TRANSFERÊNCIA
+  static const Color transferPrimaryColor = Color(0xFF1976D2); // Azul mais escuro
+  static const Color transferBarColor = Color(0xFFBBDEFB); // Azul claro para a barra
+  static const Color transferButtonColor = Color(0xFF43A047); // Verde para o botão
+  static const Color transferAccentColor = Color(0xFFC8E6C9); // Verde claro como cor de destaque
+
+  // CONTATOS
+  static const Color contactsPrimaryColor = Color(0xFF388E3C); // Verde mais escuro
+  static const Color contactsBarColor = Color(0xFFE8F5E9); // Verde claro para a barra
+  static const Color contactsButtonColor = Color(0xFF4CAF50); // Verde vibrante para o botão
+  static const Color contactsAccentColor = Color(0xFFC8E6C9); // Verde claro como cor de destaque
+}
